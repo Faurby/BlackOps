76ecae7ef4061b9ea5f88d19b946b8aab8121ea8
Faurby <faur@pop-os.localdomain>
2022-02-01 14:10:55 +0100

    small fix

--- minitwit.py
+++ minitwit.py
@@ -39,7 +39,7 @@ def init_db():
     """Creates the database tables."""
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read().encode('utf-8'))
+            db.cursor().executescript(f.read())
         db.commit()
 
 
