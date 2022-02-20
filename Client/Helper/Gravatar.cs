eb23d0873f420de13c517db5e6615c6ed24752fd
AntonBertelsen <anton.bertelsen@gmail.com>
2022-02-20 01:36:21 +0100

    Overhauled last pages. Responsiveness to different screen sizes could be improved.

--- Client/Helper/Gravatar.cs
+++ Client/Helper/Gravatar.cs
@@ -1,23 +1,17 @@
-namespace Minitwit.Client.Helper;
+namespace MiniTwit.Client.Helper;
 using MiniTwit.Shared;
 using System.Net.Http.Json;
 public class Gravatar {
     public static async Task<string> GenerateGravatarLink(HttpClient http, string UserID, int size)
     {
         var user = await http.GetFromJsonAsync<User>("api/Users/" + UserID!);
+        var utilityDTO = await http.GetFromJsonAsync<UtilityDTO>("api/Utility/md5/" + user!.Email.Trim());
 
-        if (user.Email != null) // TODO: Why is this check nescesary?
-        {
-            var utilityDTO = await http.GetFromJsonAsync<UtilityDTO>("api/Utility/md5/" + user!.Email.Trim());
+        return "https://www.gravatar.com/avatar/" + utilityDTO.hash + "?d=identicon&s=" + size;
+    }
 
-            return "https://www.gravatar.com/avatar/" + utilityDTO.hash + "?d=identicon&s=" + size;
-        }
-        else
-        {
-            var mockEmail = user.UserName + "@hotmail.com";
-            var utilityDTO = await http.GetFromJsonAsync<UtilityDTO>("api/Utility/md5/" + mockEmail.Trim());
-
-            return "https://www.gravatar.com/avatar/" + utilityDTO.hash + "?d=identicon&s=" + size;
-        }
+    public static string GetColorFromEmail(User user) {
+        var random = new Random(user.Email.GetHashCode());
+        return String.Format("#{0:X6}", random.Next(0x1000000)); // = "#A197B9"
     }
 }
