538dfc557627610acfa4daab1dc06032ede372d7
Lasse Faurby Klausen <37332429+Faurby@users.noreply.github.com>
2022-02-20 16:43:25 +0100

    Various fixes (#51)
    * Virtulization only shows as many elements that can be in the screen, and async loads new ones
    
    * Interfaces for services and made some globalUsing stuff

--- Server/Controllers/SimController.cs
+++ Server/Controllers/SimController.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
-using MiniTwit.Shared;
-
 namespace MiniTwit.Server;
 
 [ApiController]
---
+++ Server/GlobalUsing.cs
@@ -0,0 +1,7 @@
+global using Microsoft.Extensions.Options;
+global using MiniTwit.Shared;
+global using MongoDB.Driver;
+global using Microsoft.AspNetCore.Mvc;
+global using MiniTwit.Shared.SimModels;
+global using System.Diagnostics;
+global using MiniTwit.Server;
