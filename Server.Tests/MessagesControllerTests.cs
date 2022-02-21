d8ede356e8dc6a3660fec72ca47030665f09072b
Frakty <66801011+FrederikRothe@users.noreply.github.com>
2022-02-21 22:13:27 +0100

    Pumped out some more tests.  @MessagesControllerTests

--- Server.Tests/MessagesControllerTests.cs
+++ Server.Tests/MessagesControllerTests.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Moq;
 using Newtonsoft.Json;
 
@@ -40,5 +41,90 @@ public class MessagesControllerTests
         var actual = await controller.Get("1");
 
         Assert.Equal(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual.Value));
+    }
+
+
+    [Fact]
+    public async Task GetFollowingMessages_given_userID_from_followers_returns_messages()
+    {
+        var msg1 = new Message
+        {
+            Id = "1",
+            AuthorID = "1",
+            AuthorName = "Rocky",
+            Timestamp = new System.DateTime(2022,1,1),
+            Text = "Hi there, this is a test."
+        };
+        var msg2 = new Message
+        {
+            Id = "2",
+            AuthorID = "1",
+            AuthorName = "Rocky",
+            Timestamp = new System.DateTime(2022,1,1),
+            Text = "Hi there, this is a test."
+        };
+
+        var expected = new List<Message>(){msg1,msg2};
+        var repository = new Mock<IMessagesService>();
+        repository.Setup(s => s.GetMessagesFromFollowing("2")).ReturnsAsync(expected);
+        var controller = new MessagesController(repository.Object);
+
+        var actual = await controller.GetFollowingMessages("2");
+
+        Assert.Equal(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual.Value));
+
+    }
+
+
+    [Fact]
+    public async Task GetFromUserID_given_userID_returns_messages()
+    {
+        var msg1 = new Message
+        {
+            Id = "1",
+            AuthorID = "1",
+            AuthorName = "Rocky",
+            Timestamp = new System.DateTime(2022,1,1),
+            Text = "Hi there, this is a test."
+        };
+        var msg2 = new Message
+        {
+            Id = "2",
+            AuthorID = "1",
+            AuthorName = "Rocky",
+            Timestamp = new System.DateTime(2022,1,1),
+            Text = "Hi there, this is a test."
+        };
+
+
+        var expected = new List<Message>(){msg1,msg2};
+        var repository = new Mock<IMessagesService>();
+        repository.Setup(s => s.GetMessageFromUserIDAsync("2")).ReturnsAsync(expected);
+        var controller = new MessagesController(repository.Object);
+
+        var actual = await controller.GetFromUserID("2");
+
+        Assert.Equal(JsonConvert.SerializeObject(expected), JsonConvert.SerializeObject(actual.Value));
+    }
+
+    [Fact]
+    public async Task Post_returns_201Created_response()
+    {
+        var msg1 = new Message
+        {
+            Id = "1",
+            AuthorID = "1",
+            AuthorName = "Rocky",
+            Timestamp = new System.DateTime(2022,1,1),
+            Text = "Hi there, this is a test."
+        };
+
+        var repository = new Mock<IMessagesService>();
+        var controller = new MessagesController(repository.Object);
+        var actual = await controller.Post(msg1);
+
+
+        Assert.NotNull(actual);
+
     }
 }
