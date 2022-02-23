b6a1307872a555767b8664d28010ee8fd52c2e77
Faurby <lasse_fk@hotmail.com>
2022-02-23 22:07:50 +0100

    changed docker-compose, added docker files, added docker.sh and deployy.yml

---
+++ docker.sh
@@ -0,0 +1,9 @@
+#!usr/bin/env bash
+
+echo "Building images for MiniTwit"
+docker build -t faurby/webserver -f Dockerfile-webserver .
+docker build -t faurby/db -f Dockerfile-db .
+
+echo "Push images to docker hub"
+docker push faurby/webserver:latest
+docker push faurby/db:latest
