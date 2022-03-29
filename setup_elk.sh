0b8b3abbd6fee5b0bcba3ef843300ce1d5444a16
Faurby <lasse_fk@hotmail.com>
2022-03-29 15:15:12 +0200

    generic user

--- setup_elk.sh
+++ setup_elk.sh
@@ -1,6 +1,6 @@
 # Export env variables (set ELK_DIR to directory where this script is located)
 export ELK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-export ELK_USER="$( whoami )"
+export ELK_USER="blackops"
 
 echo "export ELK_DIR=$ELK_DIR" >> ~/.profile
 echo "export ELK_USER=$ELK_USER" >> ~/.profile
